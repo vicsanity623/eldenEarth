3a196c2e9fa9da15608c95fe4b63ad7360865f2f
Vic <125237471+vicsanity623@users.noreply.github.com>
2026-09-12 15:34:08 -0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Bump this version string whenever you deploy an update!
-const CACHE_NAME = 'elden-earth-v600.3.9';
+const CACHE_NAME = 'elden-earth-v600.4.3';
 
 const ASSETS_TO_CACHE = [
     './',
