08c15d7caf14d68ad4b6672de20e960e839899bb
Vic <125237471+vicsanity623@users.noreply.github.com>
2026-09-15 11:58:42 -0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Bump this version string whenever you deploy an update!
-const CACHE_NAME = 'elden-REALM-v600000';
+const CACHE_NAME = 'elden-REALM-v600001';
 
 const ASSETS_TO_CACHE = [
     './',
